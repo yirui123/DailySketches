d78b3023cec8d1e7bad5d4d563b268d35793622c
Yi Rui <>
2018-03-13 17:38:13 -0400

    new stuff

--- webgl/sketch-webgl.js
+++ webgl/sketch-webgl.js
@@ -1,62 +1,28 @@
-var angle = 0;
-var img;
-var pg;
+let angle = 0;
+let capture;
 
-function preload() {
-  img = loadImage('img1.jpg');
-}
+// function preload() {
+//   capture = createCapture(VIDEO);
+//   capture.size(320, 240);
+//   capture.hide();
+// }
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  // pg = createGraphics(256,256);
-  // ortho(-width, width, height, -height/2, 0.1, 100);
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
-  background(51);
+  background(55);
   rotateX(angle);
-  rotateY(angle/2);
-  rotateZ(angle/10);
-  translate(10, 10, 300);
-  texture(img);
-  box(30);
-  // cone(40, 100, 100);
-  // plane(30)；
-  // sphere()
-  // ellipsoid()
-  // cylinder()
-  // torus()
-  angle += 0.1
-
-  // pg.background(255);
-  // pg.text('hello world!', 50, 50);
-  // pass graphics as texture
-  // texture(pg);
-  // plane(100);
-
-  // directionalLight(r, g, b, x, y, z);
-  var dirY = (mouseY / height - 0.5) *2;
-  var dirX = (mouseX / width - 0.5) *2;
-  directionalLight(50, 100, mouseX, dirX, -dirY, 0.5);
-  ambientMaterial(50, 100, mouseX);
-  torus(100);
-  torus(200);
-  translate(11, 11, 200);
-
-  for(var i = 0; i < 500; i+=10){
-    push();
-    fill(i * 0.5, 100, 100);
-
-    //line
-    translate(0, 100, 0);
-    line(-100, 0, i, 100, 0, i);
-
-    // triangles
-    translate(11, 11, 200);
-    triangle(
-      0, sin( i + frameCount * 0.1) * 10, i,
-      60, 60, i,
-      -60, 60, i);
-
-    pop();
-  }
+  rotateY(angle / 5);
+  rotateZ(angle / 10);
+  // translate(10, 10, 10);
+  var dirX = (mouseX / width - 0.5) * 2;
+  var dirY = (mouseY / height - 0.5) * 2;
+  directionalLight(255, 100, 50, -dirX, -dirY, 0.55);
+  ambientMaterial(250);
+  // texture(capture);
+  sphere(100);
+  angle += 0.01;
+  // loadShader([vertFilename],[fragFilename])
 }
