c4070cc355ab6d68a06a8d1c0409185050d2e16c
Yi Rui <yrui@sankyinc.com>
2018-05-25 14:36:58 -0400

    ,,

--- p5/perlin-noise/perlin-noise-flow-field/sketch.js
+++ p5/perlin-noise/perlin-noise-flow-field/sketch.js
@@ -1,5 +1,5 @@
-var inc = 0.1;
-var scl = 50;
+var inc = 0.01;
+var scl = 60;
 var cols, rows;
 var fr; //framerate
 var zoff = 0;
@@ -8,49 +8,40 @@ var particles = [];
 var flowfield = [];
 
 function setup() {
-  createCanvas(600, 600);
-  background(0);
+  createCanvas(600, 600, WEBGL);
+  background(25);
+  //camera(-width, -height, -(height/2.0) / tan(PI*50.0 / 180.0), width/2, height/2, 50, 50, 600, 0);
   cols = floor(width / scl);
   rows = floor(height / scl);
-  fr = createP('');
+  // fr = createP('');
   flowfield = new Array(cols * rows);
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 100; i++) {
     particles[i] = new Particle();
   }
 }
 
 function draw() {
-  // background(255);
   var yoff = 0;
   for (var y = 0; y < rows + 2; y++) {
     var xoff = 0;
     for (var x = 0; x < cols + 2; x++) {
       var index = x + y * cols;
-      var angle = PI + noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var angle = PI + noise(xoff, yoff, zoff) * TWO_PI * 40;
       var v = p5.Vector.fromAngle(angle);
-      v.setMag(0.7);
+      v.setMag(0.3);
       flowfield[index] = v;
-      stroke(0, 2);
-      // push();
-      // translate(x * scl, y * scl);
-      // rotate(v.heading());
-      // strokeWeight(1);
-      // line(0, 0, scl, 0);
-      // pop();
       xoff += inc;
-
     }
     yoff += inc;
-    zoff += 0.001;
+    zoff += 0.01;
   }
 
   for (var i = 0; i < particles.length; i++) {
-    strokeWeight(4);
     particles[i].follow(flowfield);
     particles[i].update();
     particles[i].edges();
     particles[i].show();
+
   }
 
-  fr.html(floor(frameRate()));
 }
